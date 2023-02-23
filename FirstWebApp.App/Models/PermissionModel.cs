c8f7fe472f25c046c575939bc32132f3915b0286
arolee <125584432+arolee01@users.noreply.github.com>
2023-02-24 00:25:47 +0100

    Add files via upload

---
+++ FirstWebApp.App/Models/PermissionModel.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace;
+
+public class PermissionModel
+{
+    public int Id { get; set; }
+    public string DataDodania { get; set; }
+    public string Tytul { get; set; }
+    public string Opis { get; set; }
+    public string OdKiedy { get; set; }
+    public int DoKiedy { get; set; }
+}
