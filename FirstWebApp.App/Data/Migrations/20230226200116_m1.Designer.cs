efec96fbf3775a652cad98d4a05bbe09e05cb8bb
ZwyklySzwagier <107988928+ZwyklySzwagier@users.noreply.github.com>
2023-02-26 21:42:03 +0100

    Database Testing

--- FirstWebApp.App/Data/Migrations/20230226200116_m1.Designer.cs
+++ FirstWebApp.App/Data/Migrations/20230226200116_m1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FirstWebApp.App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstWebApp.App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230226200116_m1")]
+    partial class m1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
---
+++ FirstWebApp.App/Data/Migrations/20230226200116_m1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstWebApp.App.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnimalEntity",
+                columns: table => new
+                {
+                    IdAnimal = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AnimalDateAdded = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AnimalName = table.Column<string>(type: "TEXT", nullable: true),
+                    AnimalDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    NumberOfIndividuals = table.Column<int>(type: "INTEGER", nullable: false),
+                    Endangered = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalEntity", x => x.IdAnimal);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TreeEntity",
+                columns: table => new
+                {
+                    IdTree = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TreeDateAdded = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TreeName = table.Column<string>(type: "TEXT", nullable: true),
+                    TreeDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    LeafDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    MaxHeight = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeEntity", x => x.IdTree);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimalEntity");
+
+            migrationBuilder.DropTable(
+                name: "TreeEntity");
+        }
+    }
+}
